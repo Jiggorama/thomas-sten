e5055acdc2bd57eb7190f24f050690bab6508303
Thomas Sten <eric@Thomass-MBP.fios-router.home>
2021-02-09 17:23:21 -0500

    contact me

--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -4,7 +4,14 @@ import './Contact.css'
 const Contact = () => {
   return (
     <div className='contact'>
-      
+      <h3>Contact Me</h3>
+      <div contact-info>
+        <div>EMAIL: thomas.e.sten@gmail.com</div>
+        <div className ='contact-links'>         
+          <a href="https://www.linkedin.com/in/thomas-sten/">Linkedin</a>
+          <a href="https://github.com/Jiggorama">Github</a>
+        </div>
+      </div>      
     </div>
   );
 };
